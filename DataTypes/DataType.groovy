c8363a640d028930b52280edc218bc98fc8dbed1
v4sh6n <v4sh6n@gmail.com>
2023-03-22 17:05:32 +0100

    data variable types

---
+++ DataTypes/DataType.groovy
@@ -0,0 +1,23 @@
+class DataType{
+
+    static main(args){
+
+        byte byteVariable = 100
+        println "Byte value is" +byteVariable
+
+        short shortVariable = 1000
+        println "Short value is:" +shortVariable
+        int intVariable = 10000
+        println("Integer value is:" +intVariable)
+        long longVariable = 100000000
+        println("Long value is:" +longVariable)
+        float floatVariable = 1.5
+        println("Float value is:" +floatVariable)
+        double doubleVariable = 1.566
+        println("doubleVariable is" +doubleVariable)
+        boolean booleanVariable = true
+        println("Boolean value is:" +booleanVariable)
+        char charVariable = 'a'
+        println("char value is:" +charVariable)
+    }
+}
