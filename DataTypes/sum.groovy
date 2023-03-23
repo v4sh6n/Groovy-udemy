53369453282bba5df6edf6b86bc3ef4f3129fcfc
v4sh6n <v4sh6n@gmail.com>
2023-03-23 18:14:52 +0100

    commit

---
+++ DataTypes/sum.groovy
@@ -0,0 +1,12 @@
+// haz un programa hola mundo en groovy
+// haz un programa que sume dos numeros
+// haz un programa que sume dos numeros y muestre el resultado
+// haz un programa que sume dos numeros y muestre el resultado en una linea
+// haz un programa que sume dos numeros y muestre el resultado en una linea
+
+def num1 = 10
+def num2 = 20
+def sum = num1 + num2
+println "The sum of $num1 and $num2 is $sum"
+
+
