b74d55f8dd99dd2552d6b583bb2f1386002a6461
v4sh6n <v4sh6n@gmail.com>
2023-03-23 17:59:29 +0100

    commiteo

--- in.at.classandonjetc/Laptop.groovy
+++ in.at.classandonjetc/Laptop.groovy
@@ -28,3 +28,41 @@ class Laptop{
 
     }
 }
+
+//
+// haz codigos en groovy
+def main(args)
+{
+    println("Hello World")
+}
+
+
+class Laptop{
+
+    def price 
+    def brand 
+    def screenWorkingCondition
+    static main(args)
+    {
+        Laptop laptop = new Laptop()
+        laptop.logIn()
+        laptop.logOut()
+
+        laptop.price = 1000
+        laptop.brand = "lenovo"
+        laptop.screenWorkingCondition = true
+        
+        println(laptop.price)
+        println(laptop.brand)
+        printl(laptop.screenWorkingCondition)
+    }
+    def logIn()
+    {
+     println("System loggin in")
+    }
+    def logOut()
+    {
+        println("System loggin out")
+
+    }
+}
