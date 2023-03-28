ddaa4f50fbe6503f822a63309edc6ed2a67d9cd3
v4sh6n <v4sh6n@gmail.com>
2023-03-28 13:47:43 +0200

    commiteo

---
+++ package.demo/src/demo.groovy
@@ -0,0 +1,19 @@
+class demo {
+    
+    static void main(String[] args) {
+    
+        def array = [1, 2, 3, 4]
+    
+        for (a in array) {
+    
+            if (a == 3)
+    
+                continue
+    
+            println a
+    
+        }
+    
+    }
+}
+
