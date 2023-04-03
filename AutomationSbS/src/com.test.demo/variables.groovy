82f7ef3befb6c327d5aee20d8e793479282d7f4f
v4sh6n <v4sh6n@gmail.com>
2023-04-03 18:04:25 +0200

    Automation Step by Step Youtube Course

---
+++ AutomationSbS/src/com.test.demo/variables.groovy
@@ -0,0 +1,13 @@
+package com.test.demo
+
+def name = "Selenium"
+println "Hello ${name}"
+
+// groovy is dynamically typed language
+// so we can change the type of variable
+
+def (a,b,c,d) = [1,2,3,4]
+println a
+println b
+println c
+println d
